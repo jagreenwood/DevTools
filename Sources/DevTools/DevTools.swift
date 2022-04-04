ef9969320f48067a604ebee4c143eec1dea3f95a
Jeremy Greenwood <jeremy@mobelux.com>
2022-04-04 11:26:16 -0400

    Initial Commit

---
+++ Sources/DevTools/DevTools.swift
@@ -0,0 +1,6 @@
+public struct DevTools {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/DevToolsTests/DevToolsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DevTools
+
+final class DevToolsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DevTools().text, "Hello, World!")
+    }
+}
